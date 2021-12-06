452959ce6cfc6ab3e2b21f68afb746c47b8d6a1b
Leshan Patterson <leshan0627@gmail.com>
2021-12-06 21:43:42 +0000

    update to express_server.js to add an endpoint that shows the urlDataBase object as a json file

--- express_server.js
+++ express_server.js
@@ -9,8 +9,11 @@ const urlDatabase = {
 
 app.get("/", (req,res) => {
   res.send("hello!")
-})
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
-}) 
+});
