b03932211a5d34d8d47b82c1bc54ad6b9cafccdb
Leshan Patterson <leshan0627@gmail.com>
2021-12-06 22:04:35 +0000

    update express_server.js to add html elements to the local host page

--- express_server.js
+++ express_server.js
@@ -6,14 +6,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 app.get("/", (req,res) => {
   res.send("hello!")
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</></body></html>\n");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
